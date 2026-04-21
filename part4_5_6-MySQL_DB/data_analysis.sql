541478a54029bc079ee8d60a0eb3512309a255d3
ArchDruid19 <druidvester@gmail.com>
2026-04-21 12:49:25 -0400

    Updated ER diagram

--- part4_5_6-MySQL_DB/data_analysis.sql
+++ part4_5_6-MySQL_DB/data_analysis.sql
@@ -5,39 +5,30 @@
 -- that showcase the type of information that can be
 -- retrieved within the database
 
--- 1) Find the names of all finished products that are currently being held in inventory
+-- 1) Find the names and locations of all finished products that are currently being held in inventory
 -- This is very useful as this finds all products that customers are waiting to pickup;
 -- if the finished product is still in our inventory, then either we havn't shipped it out
--- or the customer hasn't picked it up
-SELECT i2.itemName
-FROM items i2
-WHERE i2.itemID IN
-	(SELECT inv.itemID
-	FROM inventory inv
-	WHERE inv.itemAmmount >= 1
-		AND inv.itemID IN
-			(SELECT i.itemID
-			FROM items i 
-			WHERE i.itemType = "finished product"));
-
--- 2) Find all of the item locations that contain finished products
-SELECT DISTINCT inv.itemLocation
+-- or the customer hasn't picked it up (or the database hasnt been updated)
+SELECT DISTINCT inv.itemLocation, i.itemName
 FROM items i, inventory inv
 WHERE i.itemType = 'finished product'
 	AND inv.itemID = i.itemID
+    AND inv.itemAmmount >= 1
+
+-- 2) Find the locations and names of finished products that are currently
+-- being held in inventory
+
 
 -- 3) Find names of all customers who placed in-house orders between 2025-01-20 and 2025-02-01 that cost more than $100 (inclusive)
 SELECT c.customerName
-FROM customers c
-	WHERE c.customerID IN
-	(SELECT co.customerID
-	FROM customerorders co
-	WHERE co.orderDate >= '2025-01-20' 
-		AND co.orderDate <= '2025-02-01'
-    	AND co.orderCost > 100
-    	AND co.orderType = 'in-house')
+FROM customerorders co, customers c
+WHERE co.customerID = c.customerID
+	AND co.orderDate >= '2025-01-20' 
+	AND co.orderDate <= '2025-02-01'
+	AND co.orderCost > 100
+	AND co.orderType = 'in-house';
 
--- 4) Find how many items with the name 'Brass Sheet' were added to the inventory on 2025-01-19
+-- 4) Find how many items with the name 'Brass Sheet' were transacted to the inventory on 2025-01-19
 SELECT SUM(invh.transactionAmmount) as total_brass_sheets
 FROM inventoryhistory invh
 WHERE invh.transactionDate = '2025-01-19'
